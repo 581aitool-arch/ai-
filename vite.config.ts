5ed72c5f6f6f558726111f40fc7547ed08c32bef
581aitool-arch <581.ai.tool@gmail.com>
2026-02-03 01:38:45 +0800

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // 設置為空字串或 ./ 確保資源路徑是相對於 index.html 的
   base: './',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    // 確保編譯過程不會因為小警告而停止
+    emptyOutDir: true
   }
 });
